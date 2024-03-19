11f4385c6d138536c4c87b2fb2c65f331368332c
Стас Басов <Pavel.ntg@gmail.com>
2024-03-19 15:33:15 +0500

    pytest_1

--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -1,12 +1,23 @@
-# test_routes.py
+import pytest
 from http import HTTPStatus
+from news.models import News
 
 from django.urls import reverse
 
 
-# Указываем в фикстурах встроенный клиент.
-def test_home_availability_for_anonymous_user(client):
-    # Адрес страницы получаем через reverse():
-    url = reverse('news:home')
-    response = client.get(url)
-    assert response.status_code == HTTPStatus.OK
+def test_note_exists(news):
+    notes_count = News.objects.count()
+    # Общее количество заметок в БД равно 1.
+    assert notes_count == 1
+    # Заголовок объекта, полученного при помощи фикстуры note,
+    # совпадает с тем, что указан в фикстуре.
+    assert news.title == 'Заголовок'
+
+
+# Обозначаем, что тесту нужен доступ к БД. 
+# Без этой метки тест выдаст ошибку доступа к БД.
+@pytest.mark.django_db
+def test_empty_db():
+    notes_count = News.objects.count()
+    # В пустой БД никаких заметок не будет:
+    assert notes_count == 0
